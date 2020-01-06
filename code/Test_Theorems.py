a972fd81cf241353c09e89402ecaaff3bd711d5d
lyx <liuyanxiao712@hotmail.com>
2020-01-07 00:18:06 +0800

    tests commited

---
+++ code/Test_Theorems.py
@@ -0,0 +1,11 @@
+"""
+This is a set of tests for some useful Theorems.
+Normally if tests are passed, we continue to focusing on theoretical proofs.
+"""
+
+from sympy import symbols, Matrix
+from sympy.matrices import randMatrix, zeros
+import Functional_regenerating_code_test
+from itertools import combinations
+import random
+
